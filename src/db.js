e4146997b05db8990ed5c5b20abc58fa1a0f7157
DiegoR-U <diego.ramos.lopez@ucsp.edu.pe>
2025-06-22 21:07:14 -0500

    se agrega proyecto

--- src/db.js
+++ src/db.js
@@ -13,10 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'DBP_app',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect(err => {
